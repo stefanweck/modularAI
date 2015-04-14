b7e277600240dab1ebbd3d7c2d510d6f8d8ec9a0
Stefan Weck <stefanweck1@gmail.com>
2015-04-14 12:34:52 +0200

    New Grunt build!

--- dist/modularai.js
+++ dist/modularai.js
@@ -3,7 +3,9 @@
 'use strict';
 
 //Require necessary modules
-// -- Nothing yet
+var Group = require('../entity/group.js'),
+    Entity = require('../entity/entity.js'),
+    World = require('../world/world.js');
 
 /**
  * System Constructor
@@ -16,7 +18,23 @@
  */
 var System = function(userSettings) {
 
+    /**
+     * @property {Boolean} isInitialized - True when the system is fully initialized
+     */
+    this.isInitialized = false;
 
+    /**
+     * @property {Group} entities - All the entities controlled by this system
+     */
+    this.entities = null;
+
+    /**
+     * @property {World} world - The world for demonstrating purposes
+     */
+    this.world = null;
+
+    //Load and then initialize itself
+    this.initialize();
 
 };
 
@@ -26,8 +44,43 @@ System.prototype = {
      * Initialize the system
      * @protected
      */
-    initialize: function()
-    {
+    initialize: function() {
+
+        //Check if the game is already initialized
+        if(this.isInitialized) {
+            return;
+        }
+
+        //Create a new group that will hold all entities
+        this.entities = new Group();
+
+        //Create a new entity and add it to the group
+        var firstRobot = new Entity('Robin');
+        this.entities.addEntity(firstRobot);
+
+        //Create the world object
+        this.world = new World();
+
+        //Update the system for the first time
+        this.update();
+
+        //The system is fully initialized
+        this.isInitialized = true;
+
+    },
+
+    /**
+     * All the functions that need to be executed every time the system updates
+     * Basically a game loop
+     * @protected
+     */
+    update: function() {
+
+        //Request a new animation frame and call the update function again
+        requestAnimationFrame(this.update.bind(this));
+
+        //Update the demonstration world
+        this.world.update();
 
     }
 
@@ -36,7 +89,179 @@ System.prototype = {
 //Export the Browserify module
 module.exports = System;
 
-},{}],2:[function(require,module,exports){
+},{"../entity/entity.js":2,"../entity/group.js":3,"../world/world.js":5}],2:[function(require,module,exports){
+//Because Browserify encapsulates every module, use strict won't apply to the global scope and break everything
+'use strict';
+
+/**
+ * Entity constructor
+ *
+ * @class Entity
+ * @classdesc A single entity that has certain behaviour to be executed
+ *
+ * @param {String} name - The name of this entity
+ */
+var Entity = function(name) {
+
+    /**
+     * @property {String} name - The name of this entity
+     */
+    this.name = name;
+
+    /**
+     * @property {Array} tasks - An associative array filled with all the tasks to be executed
+     */
+    this.tasks = [];
+
+};
+
+Entity.prototype = {
+
+    /**
+     * Execute all of this entities tasks
+     * @protected
+     */
+    runAllTasks: function() {
+
+        //Loop through each task and execute it
+        for(var i = 0; i < this.tasks.length; i++){
+            this.runSingleTask(i);
+        }
+
+    },
+
+    /**
+     * Execute a single task on this entity
+     * @private
+     */
+    runSingleTask: function(index) {
+
+        this.tasks[index].run();
+
+    },
+
+    /**
+     * Get a certain task from this entity
+     * @public
+     *
+     * @param {Number} index - The number of the task that is going to be returned
+     *
+     * @return {Object} The task that this entity has at the specified index
+     */
+    getTask: function(index) {
+
+        return this.tasks[index];
+
+    },
+
+    /**
+     * Add a task to this entity
+     * @public
+     *
+     * @param {Object} task - The task that is getting added to this entity
+     */
+    addTask: function(task) {
+
+        //Add the task
+        this.tasks.push(task);
+
+    },
+
+    /**
+     * Remove a task from this entity
+     * @public
+     *
+     * @param {Number} index - The number of the task that is going to be removed
+     */
+    removeTask: function(index) {
+
+        //Remove the task
+        this.task.splice(index, 1);
+
+    },
+
+    /**
+     * Count the number of tasks and return the number
+     * @public
+     *
+     * @returns {Number}
+     */
+    totalTasks: function() {
+
+        return this.tasks.length();
+
+    }
+
+};
+
+//Export the Browserify module
+module.exports = Entity;
+
+},{}],3:[function(require,module,exports){
+//Because Browserify encapsulates every module, use strict won't apply to the global scope and break everything
+'use strict';
+
+//Require necessary modules
+var Entity = require('./entity.js');
+
+/**
+ * Group constructor
+ *
+ * @class Group
+ * @classdesc The object that holds multiple entities
+ */
+var Group = function() {
+
+    /**
+     * @property {Array} entities - Collection of all the entities in this group
+     */
+    this.entities = [];
+
+};
+
+/**
+ * Function to add a new entity to this group
+ * @protected
+ *
+ * @param {Entity} entity - A reference to entity being added
+ */
+Group.prototype.addEntity = function(entity) {
+
+    //Check if the entity is the correct object
+    if(!entity instanceof Entity) {
+        return;
+    }
+
+    //Add the current entity to the list
+    this.entities.push(entity);
+
+};
+
+/**
+ * Function to remove an entity from this group
+ * @protected
+ *
+ * @param {Entity} entity - A reference to entity being removed
+ */
+Group.prototype.removeEntity = function(entity) {
+
+    //Check if the entity exists, if not, we don't have to delete it
+    var index = this.entities.indexOf(entity);
+
+    //The element doesn't exist in the list
+    if(index === -1) {
+        return;
+    }
+
+    //Remove the current entity from the group
+    this.entities.splice(index, 1);
+
+};
+
+//Export the Browserify module
+module.exports = Group;
+
+},{"./entity.js":2}],4:[function(require,module,exports){
 //Because Browserify encapsulates every module, use strict won't apply to the global scope and break everything
 'use strict';
 
@@ -55,10 +280,247 @@ var Intialize = function initializeSystem() {
 
 };
 
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function() {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function(callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
 //Initialize when fully loaded
 window.addEventListener("load", Intialize);
 
 //Export the Browserify module
 module.exports = Intialize;
 
-},{"./core/system.js":1}]},{},[2]);
+},{"./core/system.js":1}],5:[function(require,module,exports){
+//Because Browserify encapsulates every module, use strict won't apply to the global scope and break everything
+'use strict';
+
+//Require necessary modules
+// -- None yet
+
+/**
+ * World constructor
+ *
+ * @class World
+ * @classdesc The World object holds all objects that are in the demonstration world, the map etc.
+ */
+var World = function() {
+
+    /**
+     * @property {THREE.Scene} scene - Reference to the scene
+     */
+    this.scene = null;
+
+    /**
+     * @property {THREE.PerspectiveCamera} camera - Reference to the camera
+     */
+    this.camera = null;
+
+    /**
+     * @property {THREE.WebGLRenderer} renderer - Reference to the renderer
+     */
+    this.renderer = null;
+
+    /**
+     * @property {THREE.TrackballControls} controls - Reference to the controls object
+     */
+    this.controls = null;
+
+    //Initialize itself
+    this.initialize();
+
+};
+
+World.prototype = {
+
+    /**
+     * Initialize the UI elements and add them to this container
+     * @protected
+     */
+    initialize: function () {
+
+        //Initialize the camera
+        this.initializeCamera();
+
+        //Initialize the mouse controls
+        this.initializeControls();
+
+        //Create the stage, add a light and a floor
+        this.initializeStage();
+
+        //Initialize all objects in the world
+        this.initializeObjects();
+
+        //Initialize the renderer and configure it
+        this.initializeRenderer();
+
+        //Add an event listener for when the user resizes his window
+        window.addEventListener('resize', this.onWindowResize.bind(this), false);
+
+    },
+
+    /**
+     * Initialize and setup the camera
+     * @private
+     */
+    initializeCamera: function() {
+
+        //Create a new camera object
+        this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+
+        //Configure the camera
+        this.camera.position.z = 1000;
+
+    },
+
+    /**
+     * Initialize and setup the controls
+     * @private
+     */
+    initializeControls: function() {
+
+        //Create a new controls object
+        this.controls = new THREE.TrackballControls(this.camera);
+
+        //Setup the controls
+        this.controls.rotateSpeed = 1.0;
+        this.controls.zoomSpeed = 1.2;
+        this.controls.panSpeed = 1.0;
+        this.controls.noZoom = false;
+        this.controls.noPan = false;
+        this.controls.staticMoving = true;
+        this.controls.dynamicDampingFactor = 0.3;
+
+    },
+
+    /**
+     * Initialize and setup the stage
+     * @private
+     */
+    initializeStage: function() {
+
+        //Create a new scene and add an ambient light
+        this.scene = new THREE.Scene();
+        this.scene.add(new THREE.AmbientLight(0x505050));
+
+        //Create a new light
+        var light = new THREE.SpotLight( 0xffffff, 1.5 );
+
+        //Configure the light
+        light.position.set( 0, 500, 2000 );
+        light.castShadow = true;
+        light.shadowCameraNear = 200;
+        light.shadowCameraFar = this.camera.far;
+        light.shadowCameraFov = 50;
+        light.shadowBias = -0.00022;
+        light.shadowDarkness = 0.5;
+        light.shadowMapWidth = 2048;
+        light.shadowMapHeight = 2048;
+
+        //Add the light to the newly created scene
+        this.scene.add(light);
+
+        //Create a new plane, we are using this for a floor
+        var plane = new THREE.Mesh(
+            new THREE.PlaneBufferGeometry( 2000, 2000, 8, 8 ),
+            new THREE.MeshBasicMaterial( { color: 0xEDEDED } )
+        );
+
+        //Configure the plane
+        plane.receiveShadow = true;
+
+        //Add the plane to the newly created scene
+        this.scene.add( plane );
+
+    },
+
+    /**
+     * Initialize and setup the renderer
+     * @private
+     */
+    initializeRenderer: function() {
+
+        //Create a new renderer object
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
+
+        //Configure the renderer
+        this.renderer.setClearColor(0xf0f0f0);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.sortObjects = false;
+        this.renderer.shadowMapEnabled = true;
+        this.renderer.shadowMapType = THREE.PCFShadowMap;
+
+        //Append the renderer to the HTML body
+        document.body.appendChild(this.renderer.domElement);
+
+    },
+
+    /**
+     * Initialize and setup the objects
+     * @private
+     */
+    initializeObjects: function() {
+
+        //Create a reusable geometry object for a cube
+        var geometry = new THREE.BoxGeometry( 40, 40, 40 );
+
+        //Create the new cube object with the geometry
+        var object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }));
+
+        //Position the cube
+        object.position.x = 0;
+        object.position.y = 0;
+        object.position.z = 20;
+
+        //Configure the cube
+        object.castShadow = true;
+        object.receiveShadow = true;
+
+        //Add the cube to the scene
+        this.scene.add(object);
+
+    },
+
+    /**
+     * Function that is executed every time the window resizes
+     * @private
+     */
+    onWindowResize: function() {
+
+        //Change the camera's aspect rating and update it
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+
+        //Change the size of the renderer to the new window size
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    },
+
+    /**
+     * All the functions that need to be executed every time the system updates
+     * @protected
+     */
+    update: function () {
+
+        //Update the controls to match the users interaction
+        this.controls.update();
+
+        //Render the scene again
+        this.renderer.render(this.scene, this.camera);
+
+    }
+
+};
+
+//Export the Browserify module
+module.exports = World;
+
+},{}]},{},[4]);
